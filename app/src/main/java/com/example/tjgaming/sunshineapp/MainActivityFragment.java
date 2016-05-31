df14cdfa76fa284ccb15da60a437d8d5799f9a4f
tsparrow7 <tsparrow7@yahoo.com>
2016-06-01 00:47:14 +0800

    Removed Floating Action Button and created a object of View to return from the onCreateView method.

--- app/src/main/java/com/example/tjgaming/sunshineapp/MainActivityFragment.java
+++ app/src/main/java/com/example/tjgaming/sunshineapp/MainActivityFragment.java
@@ -17,6 +17,8 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return root;
     }
 }
