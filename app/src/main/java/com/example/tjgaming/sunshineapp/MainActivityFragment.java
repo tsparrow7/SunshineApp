db08d3b76435b019a362ae630e687eb809d74c6f
tsparrow7 <tsparrow7@yahoo.com>
2016-06-01 01:08:49 +0800

    added xml for as list item.  Created arrayList of fake data

--- app/src/main/java/com/example/tjgaming/sunshineapp/MainActivityFragment.java
+++ app/src/main/java/com/example/tjgaming/sunshineapp/MainActivityFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,8 +19,16 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        ArrayList<String> weatherData = new ArrayList<>();
+        weatherData.add("Mon 5/31 - Sunny - 89/75");
+        weatherData.add("Tue 6/1 - Cloudy - 81/61");
+        weatherData.add("Wed 6/2 - Sunny - 81/55");
+        weatherData.add("Thu 6/3 - Rainy - 75/57");
+        weatherData.add("Fri 6/4 - Rainy - 75/61");
+        weatherData.add("Sat 6/5 - Cloudy - 79/61");
+        weatherData.add("Sun 6/6 - Rainy - 75/63");
 
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
         return root;
     }
 }
